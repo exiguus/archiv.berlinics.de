9afba4a16c59f420e362b809b5d962ad0e09724e
Simon Gattner <github@0x38.de>
2023-12-28 21:29:09 +0100

    feat(archiv): initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Archiv berlinics.de
+
+Archives the current version of berlinics.de.
+And create a static site.
+
+Test the static site by build and run a Docker image with nginx.
+
+## Download current version
+
+```bash
+make fetch
+```
+
+## Create Docker image
+
+```bash
+make create
+make format
+make build -B
+make run
+```
+
+## Github Pages
+
+Manual deploy to github pages:
+
+```bash
+git checkout gh-pages
+make all
+# test static site
+make test
+rm -rf ./docs/** && cp -r ./build/berlinics.de/** docs/
+git add docs/
+git commit -m "feat(docs): update docs"
+git push origin gh-pages
+```
+
+## test
+
+test static site:
+
+```bash
+make test
+```
+
+update `url-list.txt` mock:
+
+```bash
+make all
+wget --recursive --spider localhost:8080 2>&1 | grep '^--' | awk '{ print $3 }' | sort | uniq > url-list.txt
+```
+
+## Prerequisites
+
+- Prettier (https://prettier.io/)
+- Git LFS (sudo apt-get install git-lfs)
+
+### LFS
+
+```bash
+git lfs track "*.jpg"
+git lfs track "*.jpeg"
+git lfs track "*.html"
+```
---
+++ input/berlinics.de/themes/berlinics/js/main.js
@@ -0,0 +1,65 @@
+/*
+ * id/version: berlinics.de v0.9
+ * autor/copyright: Simon Gattner
+ * date/location: 10-11-14 Berlin Germany
+ * contact/url: http://gattner.name/simon & http://gedit.net/
+ * licence: BY-NC-SA
+ *   http://creativecommons.org/licenses/by-nc-sa/3.0/deed.de
+ */
+// toggles
+
+function toggle()
+  {
+    var els = toggle.arguments;
+    var elsLen = els.length;
+    for (var i = 0; i < elsLen; i++)
+      {
+        var el = document.getElementById(els[i]);
+        if(el)
+          {
+             if(el.style.display != 'none')
+              {
+                el.style.display  = 'none';
+              }
+             else
+              {
+                el.style.display = '';
+              }
+          }
+      }
+  }
+// cleanUp/Change <input> etc. value
+function chgValue(obj,newValue)
+  {
+    var el = document.getElementById(obj);
+    if(el)
+      {
+        var oldValue = el.value;
+        el.onfocus = function() { if(newValue == null) { el.value = ''; } else { el.value = newValue; } }
+        el.onblur = function() { el.value = oldValue; }
+      }
+  }
+
+function _focus(obj)
+  {
+    var el = document.getElementById(obj);
+    if(el)
+      {
+        el.focus();
+      }
+  }
+
+function addClass(cl)
+ {
+  document.documentElement.className += ' '+cl;
+ }
+// onload functions
+window.onload = function outHTML()
+  {
+    addClass('jsshow'); //show js functionality
+    if(typeof window.toggleInHTML == 'function')
+      {
+        toggleInHTML();
+      }
+    searchInHTML();
+  }
